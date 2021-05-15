a56d068f45f6e8764b2ed9ecceb8ddef0d4d7589
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-05-15 13:12:46 +0530

    Added: Store the results in a Dictionary

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -3,8 +3,16 @@
 echo "Enter the three inputs :: "
 read a b c
 
+declare -A myDict
+
 result=$(($a+$b*$c))
 result1=$(($a*$b+$c))
 result2=$(($c+$a/$b))
 result3=$(($a%$b+$c))
-echo $result3
+
+myDict[1]=$result
+myDict[2]=$result1
+myDict[3]=$result2
+myDict[4]=$result3
+
+echo ${myDict[@]}
